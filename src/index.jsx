137569b1984010bc817ee8932b805d21b3e25c4e
Cassandra Wilcox <csndra0504@gmail.com>
2016-08-24 14:47:24 -0400

    feat: add socket.io-client, connect to server, set client state from server action

--- src/index.jsx
+++ src/index.jsx
@@ -2,25 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 import {createStore} from 'redux';
+import io from 'socket.io-client';
 import reducer from './reducer';
 import {Provider} from 'react-redux';
 import App from './components/App';
 import {VotingContainer} from './components/Voting';
-import {ResultsContainer) from './components/Results';
+import {ResultsContainer} from './components/Results';
 
 const store = createStore(reducer);
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-});
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`);
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+);
+
 
 const routes = <Route component={App}>
-  <Route path="/results" component={Results} />
+  <Route path="/results" component={ResultsContainer} />
   <Route path="/" component={VotingContainer} />
 </Route>;
 
